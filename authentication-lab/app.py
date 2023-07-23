360bfede5c672162cb4a50185158b7b5e768fbd1
rutsh24-meet <rut.shpigler@gmail.com>
2023-07-23 18:30:28 +0300

    Update app.py

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -6,6 +6,21 @@ app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
 
 
+config = {
+  "apiKey": "AIzaSyBhFQpx21s3QbZqYBdcCFRikoc7kW2l8uc",
+  "authDomain": "first-log-69e7d.firebaseapp.com",
+  "projectId": "first-log-69e7d",
+  "storageBucket": "first-log-69e7d.appspot.com",
+  "messagingSenderId": "963677552448",
+  "appId": "1:963677552448:web:f7bb478a9860dbafeb84a5",
+  "measurementId": "G-83FEYBYXC8",
+  "databaseURL": ""
+}
+
+
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
+
 @app.route('/', methods=['GET', 'POST'])
 def signin():
     return render_template("signin.html")
@@ -13,11 +28,21 @@ def signin():
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    return render_template("signup.html")
+   error = ""
+   if request.method == 'POST':
+       email = request.form['email']
+       password = request.form['password']
+       try:
+            login_session['user'] = auth.create_user_with_email_and_password(email, password)
+            return redirect(url_for('add_tweet'))
+       except:
+           error = "Authentication failed"
+   return render_template("signup.html")
 
 
 @app.route('/add_tweet', methods=['GET', 'POST'])
 def add_tweet():
+
     return render_template("add_tweet.html")
 
 
