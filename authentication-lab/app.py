437483f1045019401b269190ba44c9d8a7b9dd34
rutsh24-meet <rut.shpigler@gmail.com>
2023-07-24 14:16:01 +0300

    working

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -14,12 +14,14 @@ config = {
   "messagingSenderId": "963677552448",
   "appId": "1:963677552448:web:f7bb478a9860dbafeb84a5",
   "measurementId": "G-83FEYBYXC8",
-  "databaseURL": ""
+  "databaseURL": "https://first-log-69e7d-default-rtdb.europe-west1.firebasedatabase.app/"
 }
 
 
 firebase = pyrebase.initialize_app(config)
 auth = firebase.auth()
+db = firebase.database()
+
 
 @app.route('/', methods=['GET', 'POST'])
 def signin():
@@ -41,23 +43,33 @@ def signup():
    if request.method == 'POST':
        email = request.form['email']
        password = request.form['password']
+       full_name = request.form['full_name']
+       username = request.form['username']
+       bio = request.form['bio']
        try:
             login_session['user'] = auth.create_user_with_email_and_password(email, password)
+            UID = login_session['user']['localId']
+            user = {"username":username, "name":full_name, "email":email, "bio":bio, "password":password}
+            db.child("Users").child(UID).set(user)
             return redirect(url_for('add_tweet'))
        except:
            error = "Authentication failed"
-   return render_template("signup.html")
-
-
+   return render_template("signup.html", error = error)
 
 
 @app.route('/add_tweet', methods=['GET', 'POST'])
 def add_tweet():
+    if request.method == "POST":
+        return redirect(url_for('add_tweet'))
     return render_template("add_tweet.html")
+
+
+@app.route('/signout', methods=['GET', 'POST'])
+def signout():
+    print("signed out scucusasuaelefuuly")
     login_session['user'] = None
     auth.current_user = None
     return redirect(url_for('signin'))
-
 
 
 if __name__ == '__main__':
